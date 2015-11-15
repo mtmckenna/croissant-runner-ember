ca039cb3ed85f204c0820c9794c81095cc9b0b75
Matt McKenna <matt@omadahealth.com>
2015-11-15 15:32:58 -0800

    add prepend URL

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    prepend: 'https://s3.amazonaws.com/croissant.mtmckenna.com-assets/'
   });
 
   // Use `app.import` to add additional libraries to the generated
