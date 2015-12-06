f80d83072b882a602772d8081744147bfdfa7e7e
Matt McKenna <matt@mtmckenna.com>
2015-12-06 14:59:45 -0800

    remove client-side sorting

--- app/play/menu/hi-scores/route.js
+++ app/play/menu/hi-scores/route.js
@@ -7,14 +7,6 @@ export default Ember.Route.extend({
   },
 
   model: function() {
-    return this.store.query('hi-score', this.filters)
-    .then(function (data) {
-      return Ember.ArrayProxy.extend({
-        arrangedContent: Ember.computed.sort('content', 'props'),
-        props: ['createdAt:desc']
-      }).create({
-        content: data
-      });
-    });
-  },
+    return this.store.query('hi-score', this.filters);
+  }
 });
