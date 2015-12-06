dc46fa4a943ea5866522c671174d93fed62c07bf
Matt McKenna <matt@mtmckenna.com>
2015-12-06 13:52:29 -0800

    only return latest 100 scores from parse

--- app/play/menu/hi-scores/route.js
+++ app/play/menu/hi-scores/route.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  filters: {
+    limit: 100,
+    order: '-createdAt',
+  },
+
   model: function() {
-    return this.store.findAll('hi-score')
+    return this.store.query('hi-score', this.filters)
     .then(function (data) {
       return Ember.ArrayProxy.extend({
         arrangedContent: Ember.computed.sort('content', 'props'),
