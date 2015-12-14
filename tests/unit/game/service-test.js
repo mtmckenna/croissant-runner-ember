6f821261efea3f2638b41e9523d028b6f4117ea2
Matt McKenna <matt@mtmckenna.com>
2015-12-13 21:02:51 -0800

    test changeLevel, pause, and play

--- tests/unit/game/service-test.js
+++ tests/unit/game/service-test.js
@@ -1,12 +1,32 @@
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('service:game', 'Unit | Service | game', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  var service = this.subject();
-  assert.ok(service);
+test('changeLevel', function(assert) {
+  var game = this.subject();
+  game.level = 1;
+  assert.equal(game.level, 1);
+  game.changeLevel(2);
+  assert.equal(game.level, 2);
+});
+
+test('pause', function(assert) {
+  var game = this.subject();
+  var canvas = document.createElement('canvas');
+  game.configureGame(canvas);
+  assert.equal(game.paused, false);
+  game.pause();
+  assert.equal(game.paused, true);
+});
+
+test('play', function(assert) {
+  var game = this.subject();
+  var canvas = document.createElement('canvas');
+  game.configureGame(canvas);
+  assert.equal(game.paused, false);
+  game.pause();
+  assert.equal(game.paused, true);
+  game.play();
+  assert.equal(game.paused, false);
 });
