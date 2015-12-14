7b9d857537274fcc25102de08e8e16cfaabf5af1
Matt McKenna <matt@mtmckenna.com>
2015-12-14 08:11:37 -0800

    JSONSerializer is more appropriate for hi-scores

--- app/hi-score/serializer.js
+++ app/hi-score/serializer.js
@@ -1,12 +1,6 @@
-import Ember from 'ember';
 import DS from 'ember-data';
 
-export default DS.RESTSerializer.extend({
-  // Messed up to modify this hash in place...
-  serializeIntoHash: function(hash, typeClass, snapshot, options) {
-    Ember.$.extend(hash, this.serialize(snapshot, options));
-  },
-
+export default DS.JSONSerializer.extend({
   normalizeSingleResponse(store, primaryModelClass, payload) {
     payload =  { data: this.formattedRecord(payload) };
     return payload;
