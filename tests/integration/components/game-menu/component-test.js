3d1851601f9a4baedcac69e04094e17bf5e5c4c8
Matt McKenna <matt@mtmckenna.com>
2015-12-14 22:14:06 -0800

    add audio toggle test

--- tests/integration/components/game-menu/component-test.js
+++ tests/integration/components/game-menu/component-test.js
@@ -5,13 +5,11 @@ moduleForComponent('game-menu', 'Integration | Component | game menu', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  assert.expect(1);
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
+test('toggle audio', function(assert) {
   this.render(hbs`{{game-menu}}`);
 
-  assert.notEqual(this.$().text().indexOf('PLAY'), -1);
+  $('div:contains("AUDIO ON")').click();
+  assert.notEqual(this.$().text().indexOf('AUDIO OFF'), -1);
+  $('div:contains("AUDIO OFF")').click();
+  assert.notEqual(this.$().text().indexOf('AUDIO ON'), -1);
 });
