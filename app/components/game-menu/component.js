8ce1797c672f0dd93fff08bc7ca223f158c2acc3
Matt McKenna <matt@mtmckenna.com>
2015-12-25 13:54:13 -0500

    Add go back on enter

--- app/components/game-menu/component.js
+++ app/components/game-menu/component.js
@@ -10,6 +10,41 @@ export default Ember.Component.extend({
     this.set('audioEnabled', game.audioEnabled);
   },
 
+  didInsertElement() {
+    this.configureGoBackOnEnter();
+  },
+
+  willDestroyElement() {
+    this.tearDownGoBackOnEnter();
+  },
+
+  configureGoBackOnEnter() {
+    this.configureGoBackOnEnterEventListeners();
+    this.addGoBackOnEnterEventListeners();
+  },
+
+  tearDownGoBackOnEnter() {
+    this.removeGoBackOnEnterEventListeners();
+  },
+
+  configureGoBackOnEnterEventListeners() {
+    this.set('goBackOnEnter', this._goBackOnEnter.bind(this));
+  },
+
+  addGoBackOnEnterEventListeners() {
+    window.addEventListener('keydown', this.goBackOnEnter, false);
+  },
+
+  removeGoBackOnEnterEventListeners() {
+    window.removeEventListener('keydown', this.goBackOnEnter, false);
+  },
+
+  _goBackOnEnter(e) {
+    if (e.keyCode == 13) {
+      this.$().find('.js-go-back').click();
+    }
+  },
+
   audioText: Ember.computed('audioEnabled', function() {
     return ((this.get('game').audioEnabled) ? 'AUDIO OFF' : 'AUDIO ON');
   }),
