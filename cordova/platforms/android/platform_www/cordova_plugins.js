a5069aa91beaeb8c6bb97955b6fc10962f5a7973
Matt McKenna <matt@mtmckenna.com>
2016-01-02 19:18:29 -0800

    Add Android support

---
+++ cordova/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-crosswalk-webview": "1.4.0",
+    "cordova-plugin-whitelist": "1.2.0"
+}
+// BOTTOM OF METADATA
+});
