7daad5d242234d0230713104dcd56fce1e43437e
Matt McKenna <matt@mtmckenna.com>
2016-02-14 10:43:39 -0800

    clean up menu route

--- app/play/menu/route.js
+++ app/play/menu/route.js
@@ -4,18 +4,10 @@ export default Ember.Route.extend({
   game: Ember.inject.service(),
 
   pauseGame: function(){
-    const game = this.get('game');
-    game.pause();
+    this.get('game').pause();
   }.on('activate'),
 
   playGame: function(){
-    const game = this.get('game');
-    game.play();
-  }.on('deactivate'),
-
-  actions: {
-    goToLevel: function(level) {
-      this.transitionTo('play', level);
-    }
-  }
+    this.get('game').play();
+  }.on('deactivate')
 });
