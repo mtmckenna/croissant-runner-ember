8a799a73c4fe618348636cff08647f317a8e54fc
Matt McKenna <matt@mtmckenna.com>
2016-02-15 15:28:51 -0800

    fix createdAt date in hi-score

--- app/hi-score/model.js
+++ app/hi-score/model.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   initials: DS.attr('string'),
   score: DS.attr('number', { defaultValue: 0 }),
-  createdAt: DS.attr('date', { defaultValue() { return Date(); }})
+  createdAt: DS.attr('date', { defaultValue() { return new Date(); }})
 });
