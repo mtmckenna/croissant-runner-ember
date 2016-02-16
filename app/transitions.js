e410a8ccc46e730cb7d58819bf59799e35b91365
Matt McKenna <matt@mtmckenna.com>
2016-02-15 23:04:23 -0800

    clean up transitions

--- app/transitions.js
+++ app/transitions.js
@@ -1,42 +1,28 @@
 export default function(){
   this.transition(
-    this.toRoute('play'),
     this.fromRoute('index'),
+    this.toRoute('play'),
     this.use('toLeft'),
     this.reverse('toRight')
   );
 
   this.transition(
-    this.toRoute('play.menu'),
     this.fromRoute('play.index'),
+    this.toRoute('play.menu'),
     this.use('toLeft'),
     this.reverse('toRight')
   );
 
   this.transition(
-    this.toRoute('play.menu.hi-scores'),
-    this.fromRoute('play.menu.index'),
-    this.use('toLeft'),
-    this.reverse('toRight')
-  );
-
-  this.transition(
-    this.toRoute('play.menu.initials'),
-    this.fromRoute('play.menu.index'),
-    this.use('toLeft'),
-    this.reverse('toRight')
-  );
-
-  this.transition(
-    this.toRoute('play.menu.apps'),
-    this.fromRoute('play.menu.index'),
-    this.use('toLeft'),
-    this.reverse('toRight')
-  );
-
-  this.transition(
-    this.toRoute('index'),
     this.fromRoute('play.menu'),
+    this.toRoute('index'),
     this.use('toRight')
+  );
+
+  this.transition(
+    this.fromRoute('play.menu.index'),
+    this.toRoute(function(routeName){ return /^play\.menu/.test(routeName); }),
+    this.use('toLeft'),
+    this.reverse('toRight')
   );
 }
